71bbd67ec1c2403172560ee9481fca9131a30e2c
Sanghyup <sanghyeob93@naver.com>
2022-04-06 13:52:23 +0900

    push

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBi-2JDHluj6l3rWgZbJhkwjDR9K2FMZaM",
+    authDomain: "twitter-7aa83.firebaseapp.com",
+    projectId: "twitter-7aa83",
+    storageBucket: "twitter-7aa83.appspot.com",
+    messagingSenderId: "727380553645",
+    appId: "1:727380553645:web:cd81c120657f687faafb24",
+    measurementId: "G-YE83QBJ1BS"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
