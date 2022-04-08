0df71dcad783d52dc9c5926a5f7427d845440b88
Sanghyup <sanghyeob93@naver.com>
2022-04-08 11:48:38 +0900

    push

---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Routes>
+    </Router>
+  );
+};
+export default AppRouter;
